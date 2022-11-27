063186a25b25902a4394b9cd2c06a509b4c26e24
Abhisek Das <40667508+vicky1234567890000@users.noreply.github.com>
2022-11-27 14:13:50 +0530

    added subscription

---
+++ Subscription-Events/src/main/java/com/rest/api/SubscriptionEvents/SubscriptionEventsApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.SubscriptionEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionEventsApplication.class, args);
+	}
+
+}
---
+++ Subscription-Events/src/test/java/com/rest/api/SubscriptionEvents/SubscriptionEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.SubscriptionEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
